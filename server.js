b3cb65422bdef64726a5a35d9ce01e3f292dc2dd
Aryan Kumar Swain <aryannn.ks@gmail.com>
2026-02-07 12:27:08 +0530

    changes

--- server.js
+++ server.js
@@ -4,24 +4,20 @@ import previewRoutes from "./routes/preview.routes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:8080",
-    "http://localhost:5173",
-    "https://sp-backend-sq28.onrender.com",
-    "https://swainrecommend.netlify.app/"
-  ],
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8080",
+      "http://localhost:5173",
+      "https://swainrecommend.netlify.app",
+    ],
+    methods: ["GET"],
+  })
+);
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
-
 app.use("/api", previewRoutes);
 
-// ✅ IMPORTANT: Render needs process.env.PORT
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
